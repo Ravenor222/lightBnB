d429a43070d3d28d723d4179edcef07992cf0575
Nick <ndanvers222@gmail.com>
2020-02-06 23:18:18 +0000

    modularization

--- 1_queries/1sql.sql
+++ 1_queries/1sql.sql
@@ -16,11 +16,11 @@
 -- JOIN properties ON property_id = properties.id GROUP BY properties.city ORDER BY total_reservations DESC;
 
 
--- SELECT reservations.*, properties.* , avg(rating) as average_rating
--- FROM reservations
--- JOIN properties ON reservations.property_id = properties.id
--- JOIN property_reviews ON properties.id = reservations.property_id
--- WHERE reservations.guest_id = 1 AND reservations.end_date < now()::date
--- GROUP BY properties.id, reservations.id
--- ORDER BY reservations.start_date ASC
--- LIMIT 10;
+ SELECT reservations.*, properties.* , avg(rating) as average_rating
+ FROM reservations
+ JOIN properties ON reservations.property_id = properties.id
+ JOIN property_reviews ON properties.id = reservations.property_id
+ WHERE reservations.guest_id = 1 AND reservations.end_date < now()::date
+ GROUP BY properties.id, reservations.id
+ ORDER BY reservations.start_date ASC
+ LIMIT 10;
